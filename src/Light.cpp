4b472bc397c5c6048cef2269ed32e655f5b46580
greengod540 <drgamesbg@gmail.com>
2024-01-02 19:21:03 +0200

    added interpolation(my idea) for the angles and positions of lights

--- src/Light.cpp
+++ src/Light.cpp
@@ -11,8 +11,7 @@ Light::Light() {
 Light::~Light() {
 }
 
-void Light::init()
-{
+void Light::init() {
     light.shadow.texture = loadTexture("shadow.bmp");
 }
 
@@ -24,9 +23,6 @@ void Light::createLight(SDL_Rect cast_position, Uint8 r, Uint8 g, Uint8 b, Uint8
 }
 
 void Light::createShadows(SDL_Rect lightPosition, Drawable object) {
-    std::cout << SDL_GetError();
-
-
     int distanceX = lightPosition.x - object.rect.x;
     int distanceY = lightPosition.y - object.rect.y;
 
@@ -46,30 +42,30 @@ void Light::createShadows(SDL_Rect lightPosition, Drawable object) {
     int shadowPositionY;
 
     if (lightPosition.x >= Presenter::m_SCREEN_WIDTH / 2) {
-        shadowPositionX = object.rect.x - shadowWidth;
+        shadowPositionX = object.rect.x - 0.01 * (object.rect.w);
         shadowWidth++;
     }
     else {
-        shadowPositionX = object.rect.x + object.rect.w;
+        shadowPositionX = object.rect.x + object.rect.w - shadowWidth;
         shadowWidth--;
     }
 
     if (lightPosition.y >= Presenter::m_SCREEN_HEIGHT / 2) {
-        shadowPositionY = object.rect.y - shadowHeight;
+        shadowPositionY = object.rect.y - 0.01 * (object.rect.h);
         shadowHeight++;
     }
     else {
-        shadowPositionY = object.rect.y + object.rect.h;
+        shadowPositionY = object.rect.y + 0.01 * (object.rect.h);
         shadowHeight--;
     }
 
+    // Smoothly rotate the shadows
+    static double currentAngle = std::atan2(shadowPositionY, shadowPositionX) * (180.0 / M_PI);
+    double targetAngle = std::atan2(lightPosition.y - object.rect.y, lightPosition.x - object.rect.x) * (180.0 / M_PI);
+    double angle = currentAngle + 0.1 * (targetAngle - currentAngle); // ideqta za interpolato
+    currentAngle = angle;
+
     light.shadow.rect = { shadowPositionX, shadowPositionY, shadowWidth, shadowHeight };
-
-   
-    double angle = std::atan2(shadowPositionY, shadowPositionX) * (180.0 / M_PI);
-
-   
-   
 
     Presenter::drawObject(light.shadow, angle);
 }
